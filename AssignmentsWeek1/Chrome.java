9f10d75cc0988dca3eb7d4e63943d9e0d6020e72
rajcoder20 <63994147+rajcoder20@users.noreply.github.com>
2024-12-07 19:54:34 +0530

    Add files via upload
    day1Week1 Assignments

---
+++ AssignmentsWeek1/Chrome.java
@@ -0,0 +1,18 @@
+package AssignmentsWeek1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer ="Google";
+		boolean isBeta = false;
+		int releaseyear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseyear);
+		System.out.println(shortcutKey);
+	}
+
+}
